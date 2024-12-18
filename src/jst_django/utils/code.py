2e6a9c2e0be1f4f42989a8bf69815ce95201414f
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-18 14:57:34 +0500

    fix: Filter klassini o'zgartirish va xatoliklarni qayta ishlash qo'shildi

--- src/jst_django/utils/code.py
+++ src/jst_django/utils/code.py
@@ -1,6 +1,7 @@
 import isort
 from typing import Union
 import black
+from rich import print
 
 
 class Code:
@@ -10,10 +11,13 @@ class Code:
     @staticmethod
     def format_code(file_path: Union[str]) -> None:
         """Black and Isort format code"""
-        with open(file_path, "r") as file:
-            code = black.format_str(
-                isort.code(file.read(), config=isort.Config(profile="black", line_length=120)),
-                mode=black.FileMode(line_length=120),
-            )
-        with open(file_path, "w") as file:
-            file.write(code)
+        try:
+            with open(file_path, "r") as file:
+                code = black.format_str(
+                    isort.code(file.read(), config=isort.Config(profile="black", line_length=120)),
+                    mode=black.FileMode(line_length=120),
+                )
+            with open(file_path, "w") as file:
+                file.write(code)
+        except Exception as e:
+            print("[bold red]%s[/bold red]" % str(e))
